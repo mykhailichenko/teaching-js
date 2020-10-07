46ca45f5ff90ab53d9dc2a4c3c4bbef060285af0
mykhailichenko <iam.mivinka@gmail.com>
2020-10-07 13:48:54 +0300

    Add first task

---
+++ tasks/task1.js
@@ -0,0 +1,17 @@
+let users = [
+    {
+      name: "z",
+      surname: "a"
+    },
+    {
+      name: "a",
+      surname: "z"
+    },
+    {
+      name: "b",
+      surname: "b"
+    }
+  ];
+  
+  
+  console.log(users);
